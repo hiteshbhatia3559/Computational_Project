59c167917f50ae9f6d09db475323663aa37a2b1a
Hitesh <hiteshbhatia3559@gmail.com>
2019-03-15 22:57:27 +0530

    Counting and printing the figure with the mean of the data set

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import random
 import math
+import statistics
+import matplotlib.pyplot as plt
 
-N = int(round(input("How many events per sample calculation?")))
-counter_set = set()
+N = int(input("How many events per sample calculation?\n"))
+counter_set = []
 for j in range(0,101):
     counter = 0
     for i in range(0, N):
         if math.hypot(random.uniform(-1, 1), random.uniform(-1, 1)) <= 1:
             counter += 1
-    counter_set.add(counter)
-print(counter_set)
+    counter_set.append(counter)
+
+mean = statistics.mean(counter_set)
+mean_line = []
+for i in range(0,len(counter_set)):
+    mean_line.append(mean)
+
+plt.plot(counter_set,'bo')
+plt.plot(mean_line,'r+')
+plt.savefig('saved.png')

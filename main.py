fa157fac32cd909e56284cfda2877e667e99522b
Hitesh <hiteshbhatia3559@gmail.com>
2019-03-15 22:42:58 +0530

    counting

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import random
 import math
 
-n=100
-matrix = []
-
-for i in range(0,n):
-    matrix.append((random.uniform(-1,1),random.uniform(-1,1)))
-
-print(matrix)
+N = int(round(input("How many events per sample calculation?")))
+counter_set = set()
+for j in range(0,101):
+    counter = 0
+    for i in range(0, N):
+        if math.hypot(random.uniform(-1, 1), random.uniform(-1, 1)) <= 1:
+            counter += 1
+    counter_set.add(counter)
+print(counter_set)

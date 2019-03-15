94f1ea48f7d2b4c15cff7b03cd592e17f56cf86e
Hitesh <hiteshbhatia3559@gmail.com>
2019-03-15 23:58:55 +0530

    Ex1 Done

--- main.py
+++ main.py
@@ -3,20 +3,37 @@ import math
 import statistics
 import matplotlib.pyplot as plt
 
-N = int(input("How many events per sample calculation?\n"))
-counter_set = []
-for j in range(0,101):
-    counter = 0
-    for i in range(0, N):
-        if math.hypot(random.uniform(-1, 1), random.uniform(-1, 1)) <= 1:
-            counter += 1
-    counter_set.append(counter)
 
-mean = statistics.mean(counter_set)
+def calculate_plot(list_of_i):
+    dict_of_values = []
+    for N in list_of_i:
+        N = int(math.pow(2, N))
+        counter_list = []
+        for j in range(0, 101):
+            counter = 0
+            for i in range(0, N):
+                if math.hypot(random.uniform(-1, 1), random.uniform(-1, 1)) <= 1:
+                    counter += 1
+            counter_list.append(counter)
+
+        mean = statistics.mean(counter_list)
+        std_dev = statistics.stdev(counter_list)
+        uncertainity = std_dev / math.sqrt(mean)
+        dict_of_values.append({str(N):(mean,uncertainity)})
+    return dict_of_values
+
+
+calculator = calculate_plot(range(8, 14))
+
 mean_line = []
-for i in range(0,len(counter_set)):
+uncertainity_line = []
+pi_line = []
+for item in calculator:
+    mean, uncertainity = list(item.values())[0]
     mean_line.append(mean)
+    uncertainity_line.append(uncertainity)
+    pi_line.append(math.pi/4)
 
-plt.plot(counter_set,'bo')
-plt.plot(mean_line,'r+')
+plt.semilogx(mean_line,uncertainity_line)
+plt.hlines(math.pi/4,xmin=0,xmax=8192)
 plt.savefig('saved.png')
